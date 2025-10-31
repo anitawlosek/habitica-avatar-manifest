8bea601b546b2fd3159f77b0f718aa40bc76c198
anitawlosek <anita.wlosek@gmail.com>
2025-10-31 10:38:49 +0100

    fixed autumn buff name

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -18,7 +18,7 @@ export const buffItems = [
     { key: `snowball_warrior`, text: `Snowball (Warrior)`, imageFileNames: [buffImageFileNames[1]] },
     { key: `snowball_rogue`, text: `Snowball (Rogue)`, imageFileNames: [buffImageFileNames[2]] },
     { key: `snowball_healer`, text: `Snowball (Healer)`, imageFileNames: [buffImageFileNames[3]] },
-    { key: `ghost`, text: `Spooky Sparkles`, imageFileNames: [buffImageFileNames[4]] },
+    { key: `spookySparkles`, text: `Spooky Sparkles`, imageFileNames: [buffImageFileNames[4]] },
     { key: `shinySeed_wizard`, text: `Shiny Seed (Wizard)`, imageFileNames: [buffImageFileNames[5]] },
     { key: `shinySeed_warrior`, text: `Shiny Seed (Warrior)`, imageFileNames: [buffImageFileNames[6]] },
     { key: `shinySeed_rogue`, text: `Shiny Seed (Rogue)`, imageFileNames: [buffImageFileNames[7]] },
