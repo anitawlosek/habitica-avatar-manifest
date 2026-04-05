4c2b1dfe8752b83442a38621d48fe67b47dd74dd
Anita Włosek <anita.wlosek@gmail.com>
2026-04-06 00:12:59 +0200

    push dist

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+export declare const PREV_VERSION = "1.2.1";
+export declare const VERSION = "1.3.0";
+export declare const INPUT_DIR = "input";
+export declare const HABITICA_CONTENT_FILE = "input/habiticaContentFile.json";
+export declare const OUTPUT_DIR = "output/1.3.0";
+export declare const ITEMS_DETAILS_FILE = "output/1.3.0/avatarManifestItems.json";
+export declare const IMAGE_FILE_NAMES = "output/1.3.0/imageFileNames.json";
+export declare const IMAGES_META_FILE = "output/1.3.0/imagesMeta.json";
+//# sourceMappingURL=constants.d.ts.map
